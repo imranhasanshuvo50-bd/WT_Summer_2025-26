42e6dbfd793c19b44a3c6fffc428f8b06f499a99
nusrat-19 <nishat102003@gmail.com>
2026-08-25 23:48:26 +0600

    1st

--- Reciption/dashboard_reciption.php
+++ Reciption/dashboard_reciption.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>reciption</title>
+    <title>reciption  12</title>
 </head>
 <body>
     
